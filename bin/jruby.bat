0cb60ba4b91af7925fd986f2ba5a908fdcc4ecc2
toyokazu.ohara <toyokazu.ohara@ACCNF73914Q0.accenture.com>
2009-07-28 16:48:44 +0900

    support jacob lib

--- bin/jruby.bat
+++ bin/jruby.bat
@@ -13,7 +13,7 @@ IF EXIST "%~dp0_jrubyvars.bat" (set FULL_PATH=%~dp0) ELSE (set FULL_PATH=%~dp$PA
 
 call "%FULL_PATH%_jrubyvars.bat" %*
 
-if %JRUBY_BAT_ERROR%==0 "%_STARTJAVA%" %_VM_OPTS% -Xbootclasspath/a:"%JRUBY_CP%" -classpath "%CP%;%CLASSPATH%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% %_RUBY_OPTS%
+if %JRUBY_BAT_ERROR%==0 "%_STARTJAVA%" %_VM_OPTS% -Xbootclasspath/a:"%JRUBY_CP%" -classpath "%CP%;%CLASSPATH%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat -Djacob.dll.name.x86="%JRUBY_HOME%\lib\native\x86-win\jacob-1.14.3-x86.dll" -Djacob.dll.name.x64="%JRUBY_HOME%\lib\native\x64-win\jacob-1.14.3-x64.dll" org.jruby.Main %JRUBY_OPTS% %_RUBY_OPTS%
 set E=%ERRORLEVEL%
 
 call "%FULL_PATH%_jrubycleanup"
